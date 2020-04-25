c8db43200065e5556cd80593c2c3d635c2f830cf
Dimitris <iracleous@gmail.com>
2020-04-25 17:53:59 +0300

    Initial commit

---
+++ src/gr/codehub/app/Main.java
@@ -0,0 +1,25 @@
+package gr.codehub.app;
+
+public class Main {
+    public static void main(String[] args) {
+        float totalPrice;
+        //method invocation, or method call, method usage
+        totalPrice = calculation(12, 1.3f);
+        System.out.println("Total price = " + totalPrice);
+        Product product;
+        product = new Product();
+        product.setCode("A1");
+        System.out.println(product.getCode());
+    }
+
+
+    //method declaration or method definition
+    public static float calculation(int quantity, float price) {
+          float totalPrice;
+          float tax = 1.1f;
+          totalPrice = quantity * price;
+          return totalPrice + tax;
+    }
+
+
+}
---
+++ src/gr/codehub/app/Product.java
@@ -0,0 +1,17 @@
+package gr.codehub.app;
+
+public class Product {
+    private String code;
+    private String name;
+    private float price;
+    private int quantity;
+
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
